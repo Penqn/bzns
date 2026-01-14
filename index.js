b17b4012b1ce11ddf8394edfcd2efdbf9e10385c
sagarvd01 <sagarvd1995@gmail.com>
2026-01-14 14:28:26 +0530

    second commit

--- index.js
+++ index.js
@@ -1,23 +1,29 @@
-const app = require('express')();
-const path = require('path');
-const fs = require('fs');
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
 
+const app = express();
+const RES_DIR = path.join(__dirname, "res");
 
-app.get('/:biz', (req, res) => {
+app.get("/:biz", (req, res) => {
 	const biz = req.params.biz;
-	console.log(biz, __dirname);
-	const resFile = path.resolve(__dirname, `res/${biz}.html`);
-	const errFile = path.resolve(__dirname, '404.html');
 
-	if (fs.existsSync(resFile)) {
-		res.sendFile(resFile);
+	// allow only letters, numbers, hyphens
+	if (!/^[a-z0-9-]+$/i.test(biz)) {
+		return res.status(404).sendFile(path.join(__dirname, "404.html"));
 	}
-	else {
-		res.sendFile(errFile)
-	}
+
+	const filePath = path.join(RES_DIR, `${biz}.html`);
+	const errFile = path.join(__dirname, "404.html");
+
+	fs.access(filePath, fs.constants.F_OK, (err) => {
+		if (err) {
+			return res.status(404).sendFile(errFile);
+		}
+		res.sendFile(filePath);
+	});
 });
 
-
-app.listen(3001, () => {
-	console.log("listening on port");
-})
+app.listen( process.env.PORT || 3000, () => {
+	console.log(`listening on port ${process.env.PORT || 3000}`);
+});
